a5bb4531fd08317cf312b15bf3f8a46ccd003a90
Jasper Lin <cutewei.lin@gmail.com>
2025-10-21 12:24:00 +0800

    [HRSM-6] modify program.cs

--- HRS.API/Program.cs
+++ HRS.API/Program.cs
@@ -9,23 +9,25 @@ using HRS.Domain.Interfaces;
 using HRS.Infrastructure;
 using HRS.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using HRS.Shared.Core.Interfaces;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// MongoDB Configuration
+var mongoConnectionString = builder.Configuration.GetConnectionString("MongoDb")
+    ?? "mongodb://localhost:27017";
+builder.Services.AddSingleton<IMongoClient>(sp => new MongoClient(mongoConnectionString));
+builder.Services.AddScoped<MongoContext>();
+
 builder.Services.AddScoped<IUserContextService, UserContextService>();
-builder.Services.AddScoped<IJwtService, JwtService>();
-builder.Services.AddScoped<IUserVerificationService, UserVerificationService>();
-builder.Services.AddScoped<IUserSessionService, UserSessionService>();
 builder.Services.AddScoped<IItemService, ItemService>();
 builder.Services.AddScoped<IPackageService, PackageService>();
 builder.Services.AddScoped(typeof(ICrudRepository<>), typeof(CrudRepository<>));
 builder.Services.AddScoped<IItemRepository, ItemRepository>();
-builder.Services.AddScoped<IItemRateRepository, ItemRateRepository>();
 builder.Services.AddScoped<IPackageRepository, PackageRepository>();
-builder.Services.AddScoped<IPackageRateRepository, PackageRateRepository>();
 builder.Services.AddScoped<IAppConfiguration, AppConfiguration>();
 builder.Services.AddHttpContextAccessor();
 
@@ -68,8 +70,6 @@ builder.Services.AddSwaggerGen(c =>
         }
     });
 });
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddAutoMapper(cfg => { }, typeof(Program));
 
